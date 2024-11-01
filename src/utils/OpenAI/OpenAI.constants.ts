4e6586f5d305b9f7746eb9f466a430bb5e615b54
Dmitriy Che <morfeusys@gmail.com>
2024-11-01 13:57:19 +0300

    Update OpenAI.constants.ts

--- src/utils/OpenAI/OpenAI.constants.ts
+++ src/utils/OpenAI/OpenAI.constants.ts
@@ -1,24 +1,14 @@
 import { OpenAIModel } from "./OpenAI.types";
 
 export const OpenAIChatModels: Record<string, OpenAIModel> = {
-  "gpt-4": {
-    id: "gpt-4",
-    name: "GPT-4",
-    maxLimit: 8192,
+  "gpt-4o": {
+    id: "gpt-4o",
+    name: "GPT-4o",
+    maxLimit: 128000,
   },
-  "gpt-4-1106-preview": {
-    id: "gpt-4-1106-preview",
-    name: "GPT-4 (11/06)",
-    maxLimit: 8192,
-  },
-  "gpt-3.5-turbo": {
-    id: "gpt-3.5-turbo",
-    name: "GPT-3.5 Turbo",
-    maxLimit: 4096,
-  },
-  "gpt-3.5-turbo-16k": {
-    id: "gpt-3.5-turbo-16k",
-    name: "GPT-3.5 Turbo 16K",
-    maxLimit: 16384,
+  "gpt-4o-mini": {
+    id: "gpt-4o-mini",
+    name: "GPT-4o mini",
+    maxLimit: 128000,
   }
 };
